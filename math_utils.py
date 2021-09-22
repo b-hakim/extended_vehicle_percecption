run all simulation and fixed bugs in interests calculation and reorder some code

math_utils.py
@@ -87,15 +87,56 @@ def get_slope_y_intercept(line):
     return slope, c
 
 
-def in_and_near_segment(pt, line):
-    m, inter = get_slope_y_intercept(line)
+def distance_from_point_2_line(pt, line_param):
+    if line_param[0] is None:
+        return abs(line_param[1] - pt[0])
 
-    if m is None:
-        near_line = abs(pt[0] - inter) < 0.5
-    else:
-        near_line = abs(pt[1] - m * pt[0] - inter) < 0.5
+    return abs(line_param[0] * pt[0] - pt[1] + line_param[1]) / np.sqrt(line_param[0] * line_param[0] + 1)
 
-    in_segment = (line[0][0] - 0.5 < pt[0] < line[1][0] + 0.5 and line[0][1] - 0.5 < pt[1] < line[1][1] + 0.5) \
-                 or (line[1][0] - 0.5 < pt[0] < line[0][0] + 0.5 and line[1][1] - 0.5 < pt[1] < line[0][1] + 0.5)
 
-    return near_line and in_segment
+def in_segment(pt, line):
+    # line_param = get_slope_y_intercept(line)
+    # d = distance_from_point_2_line (pt, line_param)
+    # near_line = d < 0.5
+    minx, miny, maxx, maxy = min(line[0][0],line[1][0]), min(line[0][1],line[1][1]), \
+                             max(line[0][0],line[1][0]), max(line[0][1],line[1][1])
+
+    in_segment = minx - 0.5 < pt[0] < maxx + 0.5 and miny - 0.5 < pt[1] < maxy + 0.5
+
+    # return near_line and in_segment
+    return in_segment
+
+
+def in_and_near_edge(cv2x_veh_pos, edge_segments):
+    for i in range(1, len(edge_segments)):
+        line = edge_segments[i-1], edge_segments[i]
+
+        if in_segment(cv2x_veh_pos, line):
+            return True
+
+    return False
+
+
+def get_dist_from_to(pos_from, pos_to, edge_segments):
+    dist = 0
+    first_pt_found = False
+
+    for i in range(1, len(edge_segments)):
+        line = edge_segments[i-1], edge_segments[i]
+
+        if not first_pt_found:
+            if in_segment(pos_from, line):
+                first_pt_found = True
+
+                if in_segment(pos_to, line):
+                    return euclidean_distance(pos_from, pos_to)
+                else:
+                    dist += euclidean_distance(pos_from, line[1])
+        else:
+            if in_segment(pos_to, line):
+                dist += euclidean_distance(line[0], pos_to)
+                return dist
+            else:
+                dist += euclidean_distance(line[0], line[1])
+
+    assert False

vehicle_info.py
@@ -2,7 +2,9 @@ from typing import Type
 
 import traci
 
-from math_utils import euclidean_distance, angle_between_two_vectors, get_vector, does_line_intersect_polygon
+from math_utils import euclidean_distance, angle_between_two_vectors, get_vector, does_line_intersect_polygon, \
+    in_and_near_edge, get_dist_from_to
+
 import numpy as np
 
 
@@ -20,6 +22,25 @@ class Vehicle:
     @property
     def pos(self):
         return traci.vehicle.getPosition(self.vehicle_id)
+
+    @property
+    def center_pos(self):
+        def rotate_vector(vec, ceta_degree, pivot):
+            vec -= pivot.reshape((2, 1))
+            ceta_rad = np.deg2rad(ceta_degree)
+            rot_mat = np.array([[np.cos(ceta_rad), -np.sin(ceta_rad)],
+                                [np.sin(ceta_rad), np.cos(ceta_rad)]])
+            rotated_vector = np.dot(rot_mat, vec)
+            rotated_vector += pivot.reshape((2, 1))
+            return rotated_vector
+
+        center = np.array([
+            [self.pos[0], self.pos[1] - self.dimension[1]/2],
+        ])
+
+        ang = self.orientation_angle_degree - 90
+        center = rotate_vector(center.transpose(), ang, np.array(self.pos)).transpose()
+        return center[0]
 
     @property
     def speed(self):
@@ -51,16 +72,21 @@ class Vehicle:
     @staticmethod
     def dist_between_edges(first_edge, next_edge):
         r = traci.simulation.findRoute(first_edge._id, next_edge._id)
+        assert len(r.edges) == 2 or len(r.edges) == 1
         return r.length - first_edge.getLength() - next_edge.getLength()
 
     @staticmethod
-    def get_route_travel_time(route, net):
+    def get_route_travel_time(route, net, veh_pos):
         d = 0
         prev_edge = None
 
         for edge_id in route:
             curr_edge = net.getEdge(edge_id)
             d += curr_edge.getLength()
+
+            if in_and_near_edge(veh_pos, curr_edge.getShape()):
+                d -= curr_edge._length
+                d += get_dist_from_to(veh_pos, curr_edge.getShape()[-1], curr_edge.getShape())
 
             if prev_edge is not None:
                 junction_dist = Vehicle.dist_between_edges(prev_edge, curr_edge)
@@ -70,21 +96,23 @@ class Vehicle:
 
         return 3600 * (d/40000)
 
-    def get_future_route(self, net):
+    def get_future_route(self, net, time_threshold):
         full_route = traci.vehicle.getRoute(self.vehicle_id)
         current_road = traci.vehicle.getRoadID(self.vehicle_id)
 
         if current_road[0] == ":":
             current_road = self.__previous_edge_road
+            start = full_route.index(current_road) + 1
+        else:
+            start = full_route.index(current_road)
 
-        start = full_route.index(current_road)
         future_route = full_route[start:]
 
-        ## make sure this road is reacheable within 5 sec at most for 40km/h
+        ## make sure this road is reacheable within 10 sec at most for 40km/h i.e. ~111 meters
         trimmed_future_route = []
 
         for edge in future_route:
-            if Vehicle.get_route_travel_time(trimmed_future_route + [edge], net) <= 5:
+            if Vehicle.get_route_travel_time(trimmed_future_route + [edge], net, self.pos) <= time_threshold:
                 trimmed_future_route.append(edge)
             else:
                 break
@@ -92,7 +120,7 @@ class Vehicle:
         current_road = traci.vehicle.getRoadID(self.vehicle_id)
 
         if current_road[0] == ":":
-            trimmed_future_route = [current_road] + trimmed_future_route[1:]
+            trimmed_future_route = [current_road] + trimmed_future_route
 
         return trimmed_future_route
 